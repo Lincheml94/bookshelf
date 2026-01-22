0f326c040a1b8f28d33a83fa84690819583adcad
lincheml94 <lina.chemlal@gmail.com>
2026-01-22 10:15:10 +0100

    correction repository

--- code/src/pages/catalogue.tsx
+++ code/src/pages/catalogue.tsx
@@ -1,4 +1,3 @@
-
 import Fondcatalogue from "../components/fonds/fond_catalogue";
 import GalerieCatalogue from "../components/page_catalogue/galerie_catalogue";
 import Seo from "../models/props//seo/seo";
