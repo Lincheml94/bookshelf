584036f4ff0429f09bd75116c053d8ec153fb09a
lincheml94 <lina.chemlal@gmail.com>
2026-03-13 11:46:21 +0100

    css catalogue arrangé

--- code/src/components/page_catalogue/book_list_item.tsx
+++ code/src/components/page_catalogue/book_list_item.tsx
@@ -2,31 +2,30 @@
 import { Link } from "react-router";
 import styles from "../../assets/css/galerie_catalogue.module.css";
 import type { BookListItemProps } from "../../models/props/book/book_list_item_props";
+
 // récupérer les props dans les paramètres
 
+const BookListItem = ({ data }: BookListItemProps) => {
+	return (
+		<div className={styles.cartelivre}>
+			<Link to={`/book/${data.id}`}>
+				<img src={`/img/book/${data.images}`} alt={data.title} />
+				<div className={styles.legende}>
+					<p>{data.title}</p>
 
-const BookListItem = ({data}: BookListItemProps) => {
-    
-    return (
-        <div className={styles.cartelivre}>
-            <Link to={`/book/${data.id}`}>
-                <div className={styles.legende}>
-					<img src={`/img/book/${data.images}`} alt={data.title} />
-                    <p>{data.title}</p>
-                    
-                        <ul>
-                            {/* boucler sur les auteurs */}
-                            {
-                                data.authors.map((item) => (
-                                    <li key={item.id}>{item.firstname} {item.lastname}</li>))
-                            }
-            </ul>
-                    <p>prix : {data.price}€</p>
-                    </div>
-                </Link>
-				</div >
-            )
-  
+					<ul>
+						{/* boucler sur les auteurs */}
+						{data.authors.map((item) => (
+							<li key={item.id}>
+								{item.firstname} {item.lastname}
+							</li>
+						))}
+					</ul>
+					<p>prix : {data.price}€</p>
+				</div>
+			</Link>
+		</div>
+	);
 };
 
 export default BookListItem;
