9c85604417531a55d182dcacba4eef4e592ac946
lincheml94 <lina.chemlal@gmail.com>
2026-03-17 15:35:43 +0100

    clean index admin accueil, ajout des composants dans admin-layout

--- code/src/layouts/admin_layout.tsx
+++ code/src/layouts/admin_layout.tsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router";
+import style from "../assets/css/admin/dashboard.module.css";
+import DashboardHeader from "../components/admin/accueil/dashboard_header";
+import DashboardNav from "../components/admin/accueil/dashboard_nav";
 
 const AdminLayout = () => {
 	return (
 		<>
-			<Outlet />
+			<DashboardNav />
+			<div className={style["main-dashboard"]}>
+				<DashboardHeader />
+				<Outlet />
+			</div>
 		</>
 	);
 };
--- code/src/pages/admin/index.tsx
+++ code/src/pages/admin/index.tsx
@@ -1,18 +1,10 @@
 import style from "../../assets/css/admin/dashboard.module.css";
 import DashboardHeader from "../../components/admin/accueil/dashboard_header";
-import DashboardNav from "../../components/admin/accueil/dashboard_nav";
+
 import DashboardContent from "../../components/admin/accueil/dashboard-content";
 
 const Dashboard = () => {
-	return (
-		<>
-			<DashboardNav />
-			<div className={style["main-dashboard"]}>
-				<DashboardHeader />
-				<DashboardContent />
-			</div>
-		</>
-	);
+	return <DashboardContent />;
 };
 
 export default Dashboard;
