0ef84fbc4cd605ab8e81588fa6a434f88ed9f65a
lincheml94 <lina.chemlal@gmail.com>
2026-03-24 21:26:10 +0100

    responsive

--- code/src/components/admin/accueil/dashboard-content.tsx
+++ code/src/components/admin/accueil/dashboard-content.tsx
@@ -1,23 +1,34 @@
+import { use } from "react";
 import style from "../../../assets/css/admin/dashboard.module.css";
+import BookApiService from "../../../services/book_api_service";
 
 const DashboardContent = () => {
+	const results = use(new BookApiService().selectAll()).data;
+
 	return (
 		<div className={style["content-dashboard-accueil"]}>
 			<div className={style.msg}>
 				<p>Bienvenue dans votre espace personnel !</p>
 				<p>ici pour pourrez gérer le contenu de votre site</p>
 			</div>
-			<div className={style.subcontent}>
-				<div className={`${style.stat} ${style.un}`}>
-					<h2>33 livres</h2>
-				</div>
-				<div className={`${style.stat} ${style.deux}`}>
-					<h2>31 auteur.ice.s</h2>
-				</div>
-				<div className={`${style.stat} ${style.deux}`}>
-					<h2>11 évènement</h2>
-				</div>
-			</div>
+
+			{results?.map((item) => {
+				return (
+					<div className={style.subcontent} key={item.id}>
+						<div className={`${style.stat} ${style.un}`}>
+							<h2>{item.title.length} livres</h2>
+						</div>
+						<div className={`${style.stat} ${style.deux}`}>
+							{item.authors.map((item) => (
+								<h2 key={item.id}>{item.id} auteur.ice.s</h2>
+							))}
+						</div>
+						<div className={`${style.stat} ${style.deux}`}>
+							<h2>11 évènement</h2>
+						</div>
+					</div>
+				);
+			})}
 		</div>
 	);
 };
