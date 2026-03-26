d379b0f978f135bdac3965e395634277589bc141
lincheml94 <lina.chemlal@gmail.com>
2026-03-26 17:33:58 +0100

    ajout icone rayures

---
+++ code/src/components/icones/icon_stripes.tsx
@@ -0,0 +1,7 @@
+import styles from "../../assets/css/navbar.module.css";
+
+const IconStripes = () => {
+	return <img className={styles.logostripe} src="/icon_stripes.svg" alt="" />;
+};
+
+export default IconStripes;
