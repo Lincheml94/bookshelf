77f6b3d3efcd1de68b6332bb0e471d6d9448bb3d
lincheml94 <lina.chemlal@gmail.com>
2026-04-21 11:26:54 +0200

    maj

--- code/src/components/mentions_legales/mentions_legales_content.tsx
+++ code/src/components/mentions_legales/mentions_legales_content.tsx
@@ -4,73 +4,123 @@ const MentionsLegalesContent = () => {
 	return (
 		<div className={style.mentionsContainer}>
 			<h1>Mentions légales</h1>
+			<br />
+			<br />
+			<br />
 			<p>
 				Merci de lire avec attention les différentes modalités d’utilisation du
 				présent site avant d’y parcourir ses pages. En vous connectant sur ce
 				site, vous acceptez, sans réserves, les présentes modalités. Aussi,
 				conformément à l’article n°6 de la Loi n°2004-575 du 21 Juin 2004 pour
 				la confiance dans l’économie numérique, les responsables du présent site
-				internet https://bookshelf-editions.fr sont :
+				internet <a href="#">https://bookshelf-editions.fr</a> sont :
 			</p>
-
-			<p>Lina Chemlal</p>
-			<p>140 Avenue Gambetta</p>
-			<p>93170, Bagnolet</p>
-			<p>93170, Bagnolet</p>
-
+			<br />
+			<br />
+			<h2>Editeur du site</h2>
+			<br />
+			<p>raison sociale : Bookshelf Editions</p>
+			<p>Forme juridique : SARL</p>
+			<p>Siège social : 14 Avenue Gambetta, 93170 Bagnolet, France</p>
+			<p>SIRET : 852 147 963 00015</p>
+			<p>RCS : Bobigny B 852 147 963</p>
+			<p>Responsable communication : emma B.</p>
+			<p>Directeur de la publication : Emma B.</p>
+			<p>
+				Contact :{" "}
+				<a href="mailto:emma_b@bookshelfeditions.fr">
+					emma_b@bookshelfeditions.fr
+				</a>
+			</p>
+			<br />
+			<br />
 			<h2>Hebergement</h2>
-			<p>Hébergeur : OVH</p>
+			<br />
+			<p>Hébergeur : OVH, SAS, 2 rue Kellermann, 59100 Roubaix, France</p>
+			<p>Téléphone : +33 (0)9 72 10 10 07</p>
+			<p>
+				Site web : <a href="">https://www.ovh.com</a>
+			</p>
+			<br />
+			<br />
 			<h2>Conditions d'utilisation</h2>
+			<br />
 			<p>
-				Ce site https://bookshelf-editions.fr est proposé en différents langages
-				web (HTML, HTML5, Javascript, CSS, etc…) pour un meilleur confort
-				d’utilisation et un graphisme plus agréable. Nous vous recommandons de
-				recourir à des navigateurs modernes comme Internet explorer, Safari,
-				Firefox, Google Chrome, etc… https://bookshelf-editions.fr met en œuvre
-				tous les moyens dont elle dispose, pour assurer une information fiable
-				et une mise à jour fiable de ses sites internet. Toutefois, des erreurs
-				ou omissions peuvent survenir. L’internaute devra donc s’assurer de
-				l’exactitude des informations auprès de https://bookshelf-editions.fr et
-				signaler toutes modifications du site qu’il jugerait utile. n’est en
-				aucun cas responsable de l’utilisation faite de ces informations, et de
-				tout préjudice direct ou indirect pouvant en découler.
+				Ce site <a href="#">https://bookshelf-editions.fr</a> est proposé en
+				différents langages web (HTML, HTML5, Javascript, CSS, etc…) pour un
+				meilleur confort d’utilisation et un graphisme plus agréable. Nous vous
+				recommandons de recourir à des navigateurs modernes comme Internet
+				explorer, Safari, Firefox, Google Chrome ou Microsoft Edge.
+				<a href="#">https://bookshelf-editions.fr</a> met en œuvre tous les
+				moyens dont elle dispose, pour assurer une information fiable et une
+				mise à jour fiable de ses sites internet. Toutefois, des erreurs ou
+				omissions peuvent survenir. L’internaute devra donc s’assurer de
+				l’exactitude des informations auprès de
+				<a href="#">https://bookshelf-editions.fr</a> et signaler toutes
+				modifications du site qu’il jugerait utile. n’est en aucun cas
+				responsable de l’utilisation faite de ces informations, et de tout
+				préjudice direct ou indirect pouvant en découler.
 			</p>
+			<br />
+			<br />
 			<h2>Cookies</h2>
+			<br />
 			<p>
-				Le site https://bookshelf-editions.fr peut-être amené à vous demander
-				l’acceptation des cookies pour des besoins de statistiques et
-				d’affichage. Un cookie est une information déposée sur votre disque dur
-				par le serveur du site que vous visitez. Il contient plusieurs données
-				qui sont stockées sur votre ordinateur dans un simple fichier texte
-				auquel un serveur accède pour lire et enregistrer des informations.
-				Certaines parties de ce site ne peuvent être fonctionnelles sans
-				l’acceptation de cookies.
+				Le site <a href="#">https://bookshelf-editions.fr</a> peut-être amené à
+				vous demander l’acceptation des cookies pour des besoins de statistiques
+				et d’affichage. Un cookie est une information déposée sur votre disque
+				dur par le serveur du site que vous visitez. Il contient plusieurs
+				données qui sont stockées sur votre ordinateur dans un simple fichier
+				texte auquel un serveur accède pour lire et enregistrer des
+				informations. Certaines parties de ce site ne peuvent être
+				fonctionnelles sans l’acceptation de cookies. Vous pouvez refuser
+				l’utilisation des cookies en configurant votre navigateur. Pour plus
+				d’informations, consultez la politique de confidentialité du site.
 			</p>
-			<h2>Données personnelles</h2>
+			<br />
+			<br />
+			<h2>Protection des données personnelles</h2>
+			<br />
 			<p>
-				De manière générale, vous n’êtes pas tenu de nous communiquer vos
-				données personnelles lorsque vous visitez notre site internet
-				https://bookshelf-editions.fr. Cependant, ce principe comporte certaines
-				exceptions. En effet, pour certains services proposés par notre site,
-				vous pouvez être amenés à nous communiquer des données telles que :
-				votre nom, votre fonction, le nom de votre société, votre adresse
-				électronique, et votre numéro de téléphone. Tel est le cas lorsque vous
-				remplissez le formulaire qui vous est proposé en ligne, dans la rubrique
-				« contact ». Il est à noter que vous pouvez refuser de fournir vos
-				données personnelles. Dans ce cas, vous ne pourrez pas utiliser les
-				services du site, notamment celui de solliciter des renseignements sur
-				notre société, ou de recevoir les lettres d’information. Enfin, nous
-				pouvons collecter de manière automatique certaines informations vous
-				concernant lors d’une simple navigation sur notre site internet,
-				notamment des informations concernant l’utilisation de notre site, comme
-				les zones que vous visitez et les services auxquels vous accédez, votre
-				adresse IP, le type de votre navigateur, vos temps d’accès. De telles
-				informations sont utilisées exclusivement à des fins de statistiques
-				internes, de manière à améliorer la qualité des services qui vous sont
-				proposés. Les bases de données sont protégées par les dispositions de la
-				loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996
-				relative à la protection juridique des bases de données.
+				Conformément au Règlement Général sur la Protection des Données (RGPD)
+				et à la loi Informatique et Libertés, Bookshelf Editions s’engage à
+				protéger vos données personnelles. Les données collectées (nom, prénom,
+				adresse e-mail, numéro de téléphone, etc.) le sont uniquement dans le
+				cadre de la fourniture de nos services (formulaire de contact,
+				newsletter, etc.). Vous disposez d’un droit d’accès, de rectification,
+				de suppression, de limitation, d’opposition et de portabilité sur vos
+				données. Pour exercer ces droits, contactez-nous à l’adresse suivante :
+				<br />
+				<a href="mailto:emma_b@bookshelfeditions.fr">
+					emma_b@bookshelfeditions.fr
+				</a>
+				emma_b@bookshelfeditions.fr. Vos données sont conservées pour une durée
+				maximale de 3 ans à compter de leur collecte ou de votre dernier
+				contact.
 			</p>
+			<br />
+			<br />
+			<h2>Propriété intellectuelle</h2>
+			<br />
+			<p>
+				L’ensemble des contenus présents sur le site (textes, images, logos,
+				etc.) est protégé par le droit d’auteur et reste la propriété exclusive
+				de Bookshelf Editions. Toute reproduction, représentation ou diffusion,
+				même partielle, est interdite sans autorisation écrite préalable.
+			</p>
+			<br />
+			<br />
+			<h2>Litiges</h2>
+			<br />
+			<p>
+				Les présentes mentions légales sont régies par le droit français. En cas
+				de litige, les tribunaux de Bobigny seront seuls compétents.
+			</p>
+			<br />
+			<br />
+			<br />
+			<p>Dernière mise à jour : 15 avril 2026</p>
+			<br />
 		</div>
 	);
 };
